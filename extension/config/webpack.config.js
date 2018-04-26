be961a94f9f2e6c47641726a4feb8ca4429ff0c3
Alon Gamliel <alon.gamliel@fluxfederation.com>
2018-04-27 11:31:54 +1200

    genesis
    webpack configuration with typescript and react

---
+++ extension/config/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// paths
+const projectPath = path.resolve(__dirname, '..');
+const buildPath = path.join(projectPath, 'build');
+const srcPath = path.join(projectPath, 'src');
+
+const config = {
+  entry: {
+    popup: path.join(srcPath, 'popup', 'popup.tsx')
+  },
+  output: {
+    filename: '[name].js',
+    path: buildPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        include: srcPath,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: projectPath,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,   // make sure sass-loader is used on imported assets
+              localIdentName: '[local]---[hash:base64:5]'
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        include: projectPath,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']  // order is important. give typescript precedence when js file already exists.
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {from: path.join(srcPath, 'manifest.json')}
+    ]),
+    new HtmlWebpackPlugin({
+      template: path.join(srcPath, 'popup', 'popup.html'),
+      chunks: ['popup'],
+      filename: 'popup.html'
+    })
+  ],
+  devtool: 'source-map',
+  mode: 'development'
+};
+
+module.exports = config;
---
+++ extension/src/popup/popup.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import style from "./style.scss";
+
+ReactDOM.render(<div className={style.popup}>Hello World!</div>, document.getElementById('popup-root'));
